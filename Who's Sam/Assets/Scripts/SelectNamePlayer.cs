d3df5c164b5cb2e0e3ccaaf4da5a79d827679718
ElisaKalbe <elisakalbe@gmail.com>
2017-12-07 22:40:10 +0100

    #4 Add script's folder bis

---
+++ Who's Sam/Assets/Scripts/SelectNamePlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectNamePlayer : MonoBehaviour {
+
+	public List<string> playerList = new List<string>{};
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+}
