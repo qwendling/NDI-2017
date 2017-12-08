99a1cd70702b3e2fa34e8b2437cc75d3393716ba
Audric W <awasmer96@yahoo.fr>
2017-12-08 02:25:14 +0100

    #4 Players class + Menu : save players name

---
+++ Who's Sam/Assets/Scripts/Menus/PlayersManagement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayersManagement : MonoBehaviour {
+
+	// Class Player
+	public class Player {
+			
+		public string _name;
+		public int _points = 0;
+
+		public Player(string name){
+			this._name = name;
+		}
+	}
+	// Players list (static member)
+	public List<Player> Players = new List<Player>();
+	static int nbPlayers = 0;
+
+	// Reference to the input field
+	public InputField _input;
+
+	public void onEnterClicked(){
+		Players.Add (new Player (_input.text));
+		nbPlayers++;
+		_input.text = "";
+	}
+
+	public void GivePoints(int idx, int pts){
+		Players [idx]._points += pts;
+	}
+
+
+}
