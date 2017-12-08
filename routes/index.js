c7ceedc65c3d98435ec50c06c08c7d8e79d32345
nurbain <nathan.urbain@etu.unistra.fr>
2017-12-08 02:31:36 +0100

    (feat) Ajout page appication #6

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Who\'SAM' });
 });
 
 router.get('/konami', function(req, res) {
   res.render('konami', { title: 'Konami' });
+});
+
+router.get('/application', function(req, res) {
+  res.render('application', { title: 'Who\'SAM' });
 });
 
 module.exports = router;
